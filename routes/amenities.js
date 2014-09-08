15ac7d13569b13d67450d1779214ed99802f2591
Eric Theise <erictheise@gmail.com>
2014-09-08 10:58:17 -0700

    initial commit

---
+++ routes/amenities.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var tags = {};
+
+router.get('/', function(req, res) {
+  getAmenities(req.body, res);
+});
+
+function getAmenities(amenities, res) {
+  var conString = "pg://localhost/portland_from_osm";
+  var client = new pg.Client(conString);
+  client.connect();
+
+  var fc = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+
+  var sql = 'SELECT name, ST_AsGeoJSON(ST_TRANSFORM(way, 4326)) AS way, tags from planet_osm_point where amenity = \'restaurant\';';
+
+  client.query(sql, function(err, result) {
+    result.rows.forEach(function(feature){
+
+      tags = feature.tags;
+
+      var f = {
+        "type": "Feature",
+        "geometry": feature.way,
+        "properties": {
+          "name": feature.name,
+          "cuisine": getTag("cuisine")
+        }
+      };
+      fc.features.push(f);
+    });
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.send(fc);
+  });
+
+}
+
+function getTag(tag) {
+  var regexp = new RegExp('"' + tag + '"=>"(\\w+)"');
+  if (tags.match(regexp)) {
+    return tags.match(regexp)[1]
+  } else {
+    return null;
+  }
+
+}
+
+module.exports = router;
+
---
+++ test.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var pg = require("pg");
+
+var conString = "pg://localhost/portland_from_osm";
+
+var client = new pg.Client(conString);
+client.connect();
+
+// client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
+// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
+// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
+
+var query = client.query("SELECT name FROM planet_osm_point");
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    client.end();
+});
+
